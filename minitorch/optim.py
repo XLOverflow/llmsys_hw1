056f38df22b57a07634ff44c46351dc8e2bd4c55
XLOverflow <lixiang8@cmu.edu>
2026-01-29 02:21:20 -0500

    Fix Adam second moment: beta1 -> beta2

--- minitorch/optim.py
+++ minitorch/optim.py
@@ -87,7 +87,7 @@ class Adam(Optimizer):
 
                     state['step'] += 1
                     state['exp_avg'] = state['exp_avg'] * self.beta1 + (1 - self.beta1) * grad
-                    state['exp_avg_sq'] = state['exp_avg_sq'] * self.beta2 + (1 - self.beta1) * grad ** 2
+                    state['exp_avg_sq'] = state['exp_avg_sq'] * self.beta2 + (1 - self.beta2) * grad ** 2
 
                     denom = state['exp_avg_sq'] ** 0.5 + self.eps
 
